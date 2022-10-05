f66ea5aa0601e128aa370f3de08f9a5f5ba7ea2b
ajboyd2 <alexjb@uci.edu>
2022-10-05 13:51:18 -0700

    New job scripts.

---
+++ scripts/movie_marginal_query.sh
@@ -0,0 +1,37 @@
+python -u -m pp_query.evaluate \
+    --seed 123 \
+    --dont_print_args \
+    --train_data_percentage 1.0 \
+    --num_channels 182 \
+    --channel_embedding_size 32 \
+    --dec_recurrent_hidden_size 64 \
+    --dec_num_recurrent_layers 1 \
+    --dec_intensity_hidden_size 32 \
+    --dec_num_intensity_layers 2 \
+    --dec_act_func "gelu" \
+    --dropout 0.01 \
+    --checkpoint_path "./data/movie/nhp_models/" \
+    --train_epochs 100 \
+    --num_workers 2 \
+    --batch_size 128 \
+    --log_interval 500 \
+    --save_epochs 5 \
+    --optimizer "adam" \
+    --grad_clip 10000.0 \
+    --lr 0.001 \
+    --weight_decay 0.0 \
+    --warmup_pct 0.01 \
+    --lr_decay_style "constant" \
+    --train_data_path "./data/movie/train/" \
+    --valid_data_path "./data/movie/valid/" \
+    --test_data_path "./data/movie/test/" \
+    --neural_hawkes \
+    --cuda \
+    --device_num 0 \
+    --calculate_is_bounds \
+    --marginal_mark_queries \
+    --marg_query_n 3 \
+    # --just_gt \
+#    --normalize_by_window \
+#    --loss_monotonic \
+    
--- scripts/taobao_job.sh
+++ scripts/taobao_job.sh
@@ -1,17 +1,16 @@
-python -u -m pp_query.evaluate \
+python -u -m pp_query.train \
     --seed 123 \
-    --dont_print_args \
     --train_data_percentage 1.0 \
-    --num_channels 182 \
-    --channel_embedding_size 32 \
-    --dec_recurrent_hidden_size 64 \
+    --num_channels 1000 \
+    --channel_embedding_size 64 \
+    --dec_recurrent_hidden_size 128 \
     --dec_num_recurrent_layers 1 \
     --dec_intensity_hidden_size 32 \
     --dec_num_intensity_layers 2 \
     --dec_act_func "gelu" \
     --dropout 0.01 \
-    --checkpoint_path "./data/movie/nhp_models/" \
-    --train_epochs 100 \
+    --checkpoint_path "./data/taobao/nhp_models/" \
+    --train_epochs 300 \
     --num_workers 2 \
     --batch_size 128 \
     --log_interval 500 \
@@ -22,13 +21,12 @@ python -u -m pp_query.evaluate \
     --weight_decay 0.0 \
     --warmup_pct 0.01 \
     --lr_decay_style "constant" \
-    --train_data_path "./data/movie/train/" \
-    --valid_data_path "./data/movie/valid/" \
-    --test_data_path "./data/movie/test/" \
+    --train_data_path "./data/taobao/train/" \
+    --valid_data_path "./data/taobao/valid/" \
+    --test_data_path "./data/taobao/test/" \
     --neural_hawkes \
     --cuda \
-    --calculate_is_bounds \
-    --hitting_time_queries \
+    --device_num 2 \
 #    --normalize_by_window \
 #    --loss_monotonic \
     
